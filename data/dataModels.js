bc1b8f4aa3de1ab8e918de7e9b4cf5adfc53188e
TylerKingsley <tyler@kingsleyrobins.com>
2024-03-21 18:43:20 +0200

    Data model built from csv

---
+++ data/dataModels.js
@@ -0,0 +1,61 @@
+// Import csv
+const fs = require('fs');
+
+const bisTypeActsObj = {};
+const bisActs = [];
+const coverOptsByTypes = {};
+
+function readAndProcessFile(filePath, processRow) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            const rows = data.split('\n');
+            rows.forEach(row => processRow(row));
+            resolve();
+        });
+    });
+}
+
+async function dataModels() {
+    // Get Business Activities from csv
+    await readAndProcessFile('data/Business Lite Product Options.csv', (row) => {
+        // If first row skip
+        const columns = row.split(',');
+        const category = columns[0];
+        if (category === 'Category') return;
+        const thisCoverObj = {
+            option: {
+                coverName: columns[1],
+                totalPremium: columns[2],
+                stockContentLmt: columns[3],
+                barLmt: columns[5],
+                paLmt: columns[7],
+                plLmt: columns[9],
+                sasriaLmt: columns[11],
+                coverNotes: columns[13]
+            }
+        };
+        if (!coverOptsByTypes[category]) { coverOptsByTypes[category] = []; }
+        coverOptsByTypes[category].push(thisCoverObj);
+    });
+
+    // Get Cover Options from csv
+    await readAndProcessFile('data/Business Lite Business Activities.csv', (row) => {
+            const columns = row.split(',');
+            const bisType = columns[0];
+            if (bisType === "Type") return;
+            const coverOption = columns[1].replace(/\r/g, '');
+            if (!bisTypeActsObj[bisType]) {
+                bisTypeActsObj[bisType] = [];
+            }
+            bisTypeActsObj[bisType].push(coverOption);
+            bisActs.push(coverOption);
+        });
+    return { bisTypeActsObj, bisActs, coverOptsByTypes };
+}
+
+
+module.exports = dataModels;
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+
+const dataModels = require('./data/dataModels');
+
+const port = process.env.PORT || 3000;
+
+async function startServer() {
+    const { bisTypeActsObj, bisActs, coverOptsByTypes } = await dataModels();
+
+    generateAndLogDataStructure(bisTypeActsObj, bisActs, coverOptsByTypes);
+
+    app.use(express.static('public'));
+    app.use(express.static('public'));
+
+    const router = require('./router');
+
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+
+    app.use(express.static('public'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use((req, res, next) => {
+        console.log(`${new Date().toISOString()} => ${req.method} ${req.originalUrl}`);
+        next();
+    });
+
+    app.use(router);
+    
+    app.listen(port, () => console.log(`Listening on port ${port}!`))
+}
+
+startServer().catch(err => console.error("Failed to start Express server:",err));
+
+function generateAndLogDataStructure(bisTypeActsObj, bisActs, coverOptsByTypes ){
+    let bisString = "[";
+    for (const act in bisActs) {
+        bisString += (`"${bisActs[act]}",`);
+    }
+    bisString += "]";
+    console.log(`
+        {
+            "bisTypeActsObj": {
+                "desc": "This is the Business Object, it contains all the Business Activities by Type. With Type being the first key and the Business Activities being the values.",
+                "contains": ${JSON.stringify(bisTypeActsObj)}
+            },
+            "bisActs": {
+                "desc": "This is a List of all the business activities",
+                "contains": ${bisString}
+            },
+            "coverOptsByTypes": {
+                "desc": "This is the Cover Options Object, it contains all Cover Options by Type. The first key is the Type and the values are the Cover Options avaliable to that type.",
+                "contains": ${JSON.stringify(coverOptsByTypes)}
+            },
+        }`
+    );
+}
